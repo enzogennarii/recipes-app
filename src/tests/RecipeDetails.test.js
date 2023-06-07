0906a2ff28f6c1174f844a2e77bc5321801b0992
Matheus Boechat <matheusfgb@matheusfgb>
2023-06-07 13:54:09 -0300

    concluindo req29

---
+++ src/tests/RecipeDetails.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithRouter } from './helpers/renderWithRouter';
+
+import App from '../App';
+import mealsRecipesMocks from './helpers/mocks/mealsRecipes';
+import drinksRecipesMocks from './helpers/mocks/drinksRecipes';
+import corbaMock from './helpers/mocks/corbaMock';
+import ggMock from './helpers/mocks/ggMock';
+
+describe('Testes do componente RecipeDetails', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('Teste da página da refeição Corba', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => mealsRecipesMocks,
+    }));
+
+    renderWithRouter(
+      <App />,
+      { initialEntries: ['/meals'] },
+    );
+    await waitFor(() => {
+      const corba = screen.getByRole('heading', { name: /corba/i });
+      userEvent.click(corba);
+    });
+    jest.clearAllMocks();
+
+    const mockResponse = (value) => ({ json: async () => value });
+    const mockFetch = jest.fn()
+      .mockReturnValueOnce(mockResponse(drinksRecipesMocks))
+      .mockReturnValueOnce(mockResponse(corbaMock));
+    global.fetch = mockFetch;
+
+    await waitFor(() => {
+      screen.getByTestId('instructions');
+      screen.getByTestId('0-recommendation-title');
+    });
+  });
+
+  it('Teste da página do drink GG', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => drinksRecipesMocks,
+    }));
+
+    renderWithRouter(
+      <App />,
+      { initialEntries: ['/drinks'] },
+    );
+    await waitFor(() => {
+      const gg = screen.getByRole('heading', { name: /gg/i });
+      userEvent.click(gg);
+    });
+    jest.clearAllMocks();
+
+    const mockResponse = (value) => ({ json: async () => value });
+    const mockFetch = jest.fn()
+      .mockReturnValueOnce(mockResponse(mealsRecipesMocks))
+      .mockReturnValueOnce(mockResponse(ggMock));
+    global.fetch = mockFetch;
+
+    await waitFor(() => {
+      screen.getByTestId('instructions');
+      screen.getByTestId('0-recommendation-title');
+    });
+  });
+});
