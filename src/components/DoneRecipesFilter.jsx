87ff650905e62fea5888738b44ac593a078dcfa4
Ari <arisrjunior@gmail.com>
2023-06-12 19:08:12 -0300

    requisitos 44 ao 49

---
+++ src/components/DoneRecipesFilter.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+function DoneRecipesFilter({ setFilteredRecipes }) {
+  const doneRecipes = [
+    {
+      id: '52771',
+      type: 'meal',
+      nationality: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'drink',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
+
+  const handleClickBtn = ({ target }) => {
+    console.log(target.value);
+    let filteredRecipes = [];
+    if (target.value === 'Meals') {
+      filteredRecipes = doneRecipes.filter(({ type }) => type === 'meal');
+    } else if (target.value === 'Drinks') {
+      filteredRecipes = doneRecipes.filter(({ type }) => type === 'drink');
+    } else {
+      filteredRecipes = doneRecipes;
+    }
+    setFilteredRecipes(filteredRecipes);
+  };
+
+  return (
+    <div>
+      <button
+        data-testid="filter-by-meal-btn"
+        onClick={ handleClickBtn }
+        value="Meals"
+      >
+        Meals
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ handleClickBtn }
+        value="Drinks"
+      >
+        Drinks
+      </button>
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ handleClickBtn }
+      >
+        All
+      </button>
+    </div>
+  );
+}
+
+export default DoneRecipesFilter;
--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
 import Header from '../components/Header';
+import DoneRecipesFilter from '../components/DoneRecipesFilter';
+import shareIcon from '../images/shareIcon.svg';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function DoneRecipes() {
+  const [isSharedRecipe, setIsSharedRecipe] = useState(false);
+
+  const doneRecipes = [
+    {
+      id: '52771',
+      type: 'meal',
+      nationality: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'drink',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
+
+  const [filteredRecipes, setFilteredRecipes] = useState(doneRecipes);
+
+  const handleShareRecipe = (type, id) => {
+    const path = `http://localhost:3000/${type}s/${id}`;
+    copy(path);
+    setIsSharedRecipe(true);
+  };
+
   return (
     <section className="page-done-recipes">
       <Header title="Done Recipes" />
+      <DoneRecipesFilter
+        setFilteredRecipes={ setFilteredRecipes }
+      />
+      { filteredRecipes.map((recipe, index) => (
+        <div key={ index }>
+          <Link
+            to={ `/${recipe.type}s/${recipe.id}` }
+          >
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt={ recipe.name }
+              className="recipe-img"
+            />
+          </Link>
+          <h3
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { recipe.type === 'meal' ? (
+              <div>
+                { `${recipe.nationality} - ${recipe.category}` }
+              </div>
+            ) : (
+              <div>
+                { recipe.alcoholicOrNot }
+              </div>
+            ) }
+          </h3>
+          <Link
+            to={ `/${recipe.type}s/${recipe.id}` }
+          >
+            <h3
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { recipe.name }
+            </h3>
+          </Link>
+          <h3
+            data-testid={ `${index}-horizontal-done-date` }
+          >
+            { recipe.doneDate }
+          </h3>
+          <button
+            src={ shareIcon }
+            data-testid={ `${index}-horizontal-share-btn` }
+            onClick={ () => handleShareRecipe(recipe.type, recipe.id) }
+          >
+            Share
+          </button>
+          { isSharedRecipe && (
+            <h3>Link copied!</h3>
+          ) }
+          {recipe.tags.map((tagName) => (
+            <h3
+              key={ tagName }
+              data-testid={ `${index}-${tagName}-horizontal-tag` }
+            >
+              { tagName }
+            </h3>
+          ))}
+        </div>
+      )) }
     </section>
   );
 }
